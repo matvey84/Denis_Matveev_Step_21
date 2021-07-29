9d344861ce9aac008d33be39e3fe55631c0fe34d
matvey84 <matvey8407@mail.ru>
2021-07-29 21:28:50 +0300

    add exesice

---
+++ js/script.js
@@ -0,0 +1,40 @@
+/*let boderLenght = +prompt('Введите длину сорроны');
+
+ 
+alert(boderLenght**2)*/
+
+
+/*const mile = 0.621371;
+let km = +prompt('Ведите км ');
+alert(`mile:${km*mile}`)*/
+
+/*let numberOne = +prompt('ВВедите число 1');
+let numberTwo = +prompt('ВВедите число 2');
+alert(`${numberOne+numberTwo}`);
+alert(`${numberOne-numberTwo}`);
+alert(`${numberOne*numberTwo}`);
+alert(`${numberOne/numberTwo}`);*/
+/*
+let numberOne = +prompt('Введите число 1');
+let numberTwo = +prompt('Введите число 2');
+alert(`${-numberTwo/numberTwo}`);*/
+
+ /*let hours = +prompt(`Введиите часы`);
+ let minutes = +prompt(`ВВедите минуты`);
+if (hour === 24) {
+alert(`Новый день`)
+} else if (hours === 0 && minutes === 0){
+   alert(`До следующего дня 24 часа`) 
+}
+alert(`${23-hours} часа - ${60-minutes} минут`);*/
+/*
+let number = +prompt(`Введите трехзначное число`);
+alert(`${(Math.floor((number % 100)/10))}`);*/
+
+
+let number = +prompt(`Введите пятизначное число`);
+
+
+
+
+
