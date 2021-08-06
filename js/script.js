fe1206de3afb22ca7c3010f3a6a37992d1de78be
matvey84 <matvey8407@mail.ru>
2021-08-06 10:23:07 +0300

    add change to clsswork#3

--- js/script.js
+++ js/script.js
@@ -32,9 +32,93 @@ let number = +prompt(`Введите трехзначное число`);
 alert(`${(Math.floor((number % 100)/10))}`);*/
 
 
-let number = +prompt(`Введите пятизначное число`);
+ //----------------------------Классные занятия 05.08.2021 Пракическое задание № 3
+
+//Задача №1
+//Задача №2
+/*
+let num = +prompt('Vvedite chislo:');
+let i = 0
+while (i <= num){
+   document.write(num);
+   num--;
+}*/
+//Задача №3
+/*
+let num = +prompt('Vvedite chislo:');
+let degree = +prompt('Vvedite stepen:');
+let result = 1;
+let i = 1;
+while (i<=degree) {
+result *=num;   
+i++;
+}
+document.write(result);
+*/
+//Задача №4
+/*
+let num = +prompt('Vvedite pervoe chislo:');
+let num2 = +prompt('Vvedite vtoroe chislo:');
+let i = num > num2 ? num2 : num;
+while(i>=0){
+   if(num%i===0){
+      if(num2%i===0){
+         document.write(i);
+      }
+   }
+   i--;
+}
+*/
+/*
+//Задача №5// ne rabotaet
+let num = +prompt('Vvedite chislo:');
+let result = 1;
+while(num<=1) {
+    result= result*num; 
+    num--;
+    document.write(`${result}`);
+}
+*/
+//------------ do..while
+//Задача №1
+/*
+let ans;
+do{
+   ans = +prompt('2+2*2');
+}while(ans!==6);
+document.write('Pravilno!');
+*/
+//Задача №2
+/*
+let num = 500;
+let count=0;
+do{
+   num= num/2;
+   count++;
+}while(num>50);
+document.write(num,  count);
+  40  js/scripthome.js 
+@@ -300,16 +300,44 @@ if (squareSide>=diametr){
+}else {alert('Okruzhnost slishkom bolshaya!');
+}
+*/
+	//ЗАДАЧА №9---- ispolzovat confirm!!!!!!!!!!!!!!!
+//----------------------------------------------ЗАДАЧА №9
+/*
+let question1 = prompt('sKOLKO BUDET 1+1?');
+if (question1){
+	let answer = confirm(2);
+	confirm(3);
+	confirm(4);
+}
+let question2 = prompt('sKOLKO BUDET 1+2?');
+let question3 = prompt('sKOLKO BUDET 1+3?');
+*/
+
+	//ЗАДАЧА №10
 
 
 
+ 
 
 
