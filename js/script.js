fa878869a6aaf34f4a97f84a9ed5d71f5e2eccd9
matvey84 <matvey8407@mail.ru>
2021-08-09 21:20:17 +0300

    add class work

--- js/script.js
+++ js/script.js
@@ -31,10 +31,74 @@ alert(`${23-hours} часа - ${60-minutes} минут`);*/
 let number = +prompt(`Введите трехзначное число`);
 alert(`${(Math.floor((number % 100)/10))}`);*/
 
+//--------------------------классные задания 09.08.2021
+/*
+let num = +prompt('jhfjf');надо ввести 5 чисел  вывести х с конца
+let i=5;//итерации должно быть 5 раз 
+while(i!=1){//
 
-let number = +prompt(`Введите пятизначное число`);
+ alert(i--); 
+}*/
+//--------------- tabl umnozheniya na 3
+/*let multipl = 3;множитель
 
-
-
-
-
+let result = 0; запсывает сюда результат
+let i = 1; цфра которую умножают
+while (i<10) {
+   result = i*multipl;
+   i++;
+  
+  document.write(result);
+}
+*/
+//-----------------------------------------for
+/*for(i=0; i<5; i++)=>
+let i=0;
+while(i<5){
+   i++;
+}*/
+ //---------------------------------------------------------
+ //let num = +prompt(' ');//ввести делитель
+/* for(i=1;i<=100;i++){// 
+if (i%num==0){
+   document.write(i); 
+}
+ }*/
+ // или через while
+/* let i=1;
+ while(i<=100){
+   if (i%num==0){
+      document.write(i);
+ }
+ i++;
+}*/ 
+//------------------------------vivesti kazhdoe 4 chislo iz diapozona
+/*let num1 = +prompt('min ');//1
+let num2 = +prompt('max ');//10
+let a; //menshee
+let b;// bolshee
+count = 1;//peremennaya schetchika
+if (num1>num2){//определение какое из чисел меньшее(диапозон)
+   b =num1;
+   a =num2;
+} else{
+   b= num2;
+   a= num1;
+} 
+for(a;a<b;a++){// vedenie kazdogo 4go chisla
+   if(count%4===0){
+      document.write(a);
+   }
+   count++;
+}
+*/
+let num = +prompt('chislo');
+let prostoe = true;
+for(let i=2;i<num;i++){
+if (num%i ===0){
+   prostoe=false;
+  
+   break;
+};
+}
+prostoe? alert('prostoe')('neprostoe')
