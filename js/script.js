dd4e5e5618f0f56e4c97fae512dbb3b5741d27a7
matvey84 <matvey8407@mail.ru>
2021-08-12 14:22:16 +0300

    add practice home sad class work

--- js/script.js
+++ js/script.js
@@ -92,13 +92,162 @@ for(a;a<b;a++){// vedenie kazdogo 4go chisla
    count++;
 }
 */
-let num = +prompt('chislo');
-let prostoe = true;
-for(let i=2;i<num;i++){
-if (num%i ===0){
-   prostoe=false;
-  
-   break;
-};
+/*
+let num = +prompt('3424'); //5
+let Prostoe = true;
+for(let i= 2; i<num; i++) {
+    if(num % i === 0) {
+        Prosto = false;
+        break;
+    };
 }
-prostoe? alert('prostoe')('neprostoe')
+
+Prostoe ? console.log('prostoe') : console.log('neprostoe')
+*/
+//-------------------------------------практическое задание №3
+
+//-----------------------------------ЗАДАЧА №1(ok!)
+/*let num= +prompt('chislo');
+let i =1;
+while(i<=num){
+	document.write( ' # ' );
+		i++;
+}*/
+//-----------------------for
+/*for(let i=1;i<=num;i++){
+	document.write(' # ');
+}*/
+
+
+//-----------------------------------ЗАДАЧА №2(ok!)
+/*let num= +prompt('chislo');
+let i=num;
+while(i>=0){
+	document.write(i);
+	i--;
+}*/
+//--------------- for
+/*
+for (let i= num; i>=0;i--){
+	document.write(`${i}, `);}
+	*/
+	
+//-----------------------------------ЗАДАЧА №3
+/*let num=+prompt('chislo');
+let step=+prompt('stepen');
+let result = 1;
+let i =1;
+
+while(i<=step){
+	result*=num;
+	i++;
+}document.write(result);
+*/
+//--------------------------ЗАДАЧА №4
+/*let num1=+prompt('chislo1');//20
+let num2=+prompt('chislo2');//2
+//let i = 0;
+let result =0; 
+let finResult;
+let max = num1>num2 ? num1:num2;
+let min = num2<num1 ? num2 :num1;
+
+//num1>num2 ? num2:num1;
+while(max>=1){
+result = max/min;
+result =(min/result);//5
+finResult= result/result;
+document.write(finResult);
+max--;
+console.log(finResult);
+}*/
+
+
+//-------------------pravilniy otvet
+/*let chislo1 = +prompt('Vvedite chislo 1');
+let chislo2 = +prompt('Vvedite chislo 2');
+let i = chislo1 > chislo2 ? chislo2 : chislo1;
+while (i>=1) {
+    if(chislo1 % i === 0) {
+        if(chislo2 % i === 0) {
+            document.write(`${i} `);
+        }
+    }
+    i--;
+}*/
+//--------------------------ЗАДАЧА №5(ok!)
+/*
+let num=+prompt('chislo');
+
+let i=1;
+let result=1;
+
+while(i<=num){
+	result*=(i);//=> factorial kazhdogo chisla 1=2;2=4;3=6;4=24;5=120;6=720;....
+
+i++;
+}	document.write(`${result} `);//esli vyvod vnesti v skobki nto srabotaet cikl i vyvedetto cto v verhnem komentarii
+*/
+//------------------------------------(DO...WHILE)
+//--------------------------ЗАДАЧА №1(ok!)
+/*let question; 
+let ans = 6;
+do {
+question= +prompt('пример:2+2*2');
+if(question===ans){
+	break;			
+	}
+ }while(ans===6)
+ document.write('Pravilno!');
+*/
+//--------------------------ЗАДАЧА №2(--)
+/*
+let num =1000;
+let num2 = 2;
+let i = 1;
+let result=0;
+
+do {
+		num=num/num2;
+		
+		console.log(num);
+
+		
+}while(num>=50)
+		
+console.log(num2);
+*/
+//--------------------------------------for
+//--------------------------ЗАДАЧА №3(+)
+/*
+let a = +prompt('delitel');
+for(i=1; i<=100;i++){
+if (i%a===0){
+
+	document.write(`${i}, `);
+}
+}
+*/
+//--------------------------ЗАДАЧА №4(ok+-)
+/*let num1 = +prompt('chislo1');
+let num2 = +prompt('chislo1');
+result=0;
+let i=num1<num2 ? num1:num2;// diapozon!!!!!!!!!!! (-)
+for(let i=1;i<=num2;i++){
+	if(result=i%4===0){
+		document.write(`${i} `);
+	}
+  }*/
+	//--------------------------ЗАДАЧА №5
+	let num = +prompt('chislo1');
+	let result;;//= (num%num===0)&&num%2===0 ? true:false;
+	for(let i= 2; i<num; i++){
+
+	if (num%i===0){
+		result=false;
+				break;
+	};//else{result=true;
+	//}
+}
+result ? document.write('neprostoe'):document.write('prostoe');
+	
